3fbe02119219bae049a669440228c630fc4a3aa6
Kazen Gallman <kazar444@icloud.com>
2021-07-12 14:46:08 -0400

    fixing comment

---
+++ percent_data/get_percent_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import sqlite3
+
+#'data-by-modzcta.csv' taken from:
+# https://github.com/nychealth/coronavirus-data/blob/master/totals/data-by-modzcta.csv
+
+#Goals
+"""
+Covid Cases:
+    Get csv from file and convert to Dataframe
+    Clean data
+    Formate data to be stored into the sql
+"""
+
+# Run in Main Directory - python percent_data/get_percent_data.py
+data_path = 'percent_data\data-by-modzcta.csv'
+sql_path = 'final_project.db'
+data = pd.read_csv(data_path)
+
+# New Dataframe 
+df = []
+df = pd.DataFrame([], columns = ["ZIP", "COVID_CASE_PERCENT"])
+
+df["ZIP"] = data["MODIFIED_ZCTA"].astype(str)
+df["COVID_CASE_PERCENT"] = data["COVID_CASE_RATE"].astype(float)
+
+# Since rate is per 100,000
+# Converting it to percentage needs to divide the rate by 1000
+# Rounded up to the 5th decimal place
+df["COVID_CASE_PERCENT"] = df["COVID_CASE_PERCENT"].apply(lambda rate: round(rate / 1000, 5))
+# print(df[:5])
+
+# Stores the frame into the sql table
+conn = sqlite3.connect(sql_path)
+df.to_sql('covid_percent', conn, if_exists='replace', index=False)
+conn.close()
